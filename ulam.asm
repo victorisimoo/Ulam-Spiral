2d623e7b74f351405ed6f0ed96056aee0d157a78
victorisimoo <me@victorisimo.com>
2021-09-24 11:00:34 -0600

    obtaining numbers and calcultate hundreds

--- ulam.asm
+++ ulam.asm
@@ -1,6 +1,10 @@
 .model small
 .data 
-    ulamelength db ?
+    ; number framing variables
+    units db 0
+    tens db 0
+    hundreds db 0
+    completenumber db 0
     msg0 db 10, 13, 7, 'Enter the desired length of the spiral: ', '$'
 .stack
 .code
@@ -14,11 +18,44 @@
         lea dx, msg0
         int 21h
 
+        ; hundreds
         mov ah, 01h
         int 21h
         sub al, 30h
-        mov ulamelength, al
+        mov hundreds, al
 
+        ; tens
+        mov ah, 01h
+        int 21h
+        sub al, 30h
+        mov tens, al
+
+        ; units
+        mov ah, 01h
+        int 21h
+        sub al, 30h
+        mov units, al
+        
+        mov al, hundreds
+        mov bl, 100
+        mul bl
+        mov completenumber, al
+
+        mov al, completenumber
+        mov bl, 100
+        div bl
+        mov hundreds, al
+
+        mov dl, hundreds
+        add dl, 30h
+        mov ah, 02h
+        int 21h
+
+        ;printing of an enter
+        mov dl, 0Ah
+        int 21h
+
+        ; finish program
         mov ah, 4ch
         int 21h
 end ulam
